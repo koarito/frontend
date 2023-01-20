576f5c17bcf588ed3231c2021d035161eca7c50e
koarito <amiromarhonein@hotmail.fr>
2023-01-20 17:08:30 +0100

    x

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 function App() {
+
+  const [weatherInfo, setWeatherInfo] = useState([]);
+  useEffect(() =>{
+    loadWeather();
+  })
+  const loadWeather = async () => {
+    const result = await axios.get("http://localhost:8080/weather/info?latitude=31.63&longitude=-8.00", {}, {auth: {
+      username: "user",
+      password: "4e00bbf4-4879-4161-aa1c-610d6aa3ebda"
+    }});
+    setWeatherInfo(result.data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Stack direction="row" spacing={2}>
+   <Button variant ="contained" onClick={() => {console.log(weatherInfo)}}>{weatherInfo.latitude}</Button>
+      
+     
+    </Stack>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const themeOptions = {
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#1976d2',
+      },
+      secondary: {
+        main: 'rgb(220, 0, 78)',
+      },
+      background: {
+        default: '#fff',
+        paper: '#fff',
+      },
+    },
+  };
