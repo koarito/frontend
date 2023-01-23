12eb4106f1f0d16e303b26345bed7a38ac370b0a
koarito <amiromarhonein@hotmail.fr>
2023-01-23 16:33:40 +0100

    minor changes

--- src/Weather.js
+++ src/Weather.js
@@ -17,63 +17,51 @@ import TextField from "@mui/material/TextField";
 
 function Weather() {
   const [weatherInfo, setWeatherInfo] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("Marrakech");
+  var cityLatitude = new Map([
+    ["Marrakech", 31.63],
+    ["Stockholm", 59.33],
+    ["Berlin", 52.52]
+  ])
+  var cityLongitude = new Map([
+    ["Marrakech", -8.00],
+    ["Stockholm", 18.07],
+    ["Berlin", 13.41]
+  ])
+  
   useEffect(() => {
     loadWeather();
   });
   const loadWeather = async () => {
-    const result = await axios.get("http://localhost:8080/weather/info?latitude=31.63&longitude=-8.00", {}, {auth: {
+    const result = await axios.get(`http://localhost:8080/weather/info?latitude=${cityLatitude.get(selectedCity)}&longitude=${cityLongitude.get(selectedCity)}`, {}, {auth: {
       username: "user",
       password: "fc5ad5bb-4447-4054-86d4-c1944366c0df"
     }});
     setWeatherInfo(result.data);
   }
+
+  const handleSetCity = () => {
+
+  }
   return (
     <div className="App">
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="Latitude" variant="outlined" />
-        <TextField id="outlined-basic" label="Longitude" variant="outlined" />
-      </Box>
-
       <Stack direction="row" spacing={2}>
-        <Button
-          variant="contained"
-          onClick={() => {
-            console.log(weatherInfo);
-          }}
-        >
-          {weatherInfo.latitude}
-        </Button>
+
+        <Button variant="contained" onClick={() => {console.log(weatherInfo)}}>Set City</Button>
       </Stack>
 
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Weather for your chosen city</TableCell>
-              <TableCell align="right">latitude</TableCell>
-              <TableCell align="right">longitude</TableCell>
+              <TableCell align="right">time</TableCell>
               <TableCell align="right">temperature</TableCell>
-              <TableCell align="right">rain</TableCell>
               <TableCell align="right">wind direction</TableCell>
               <TableCell align="right">windspeed</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 }  }}
-            >
-              <TableCell component="th" scope="row">
-                {weatherInfo.latitude}
-              </TableCell>
-              <TableCell align="right">{weatherInfo.latitude}</TableCell>
-              <TableCell align="right">{weatherInfo.longitude}</TableCell>
+            <TableRow>
               <TableCell align="right">{weatherInfo.temperature_2m}</TableCell>
               <TableCell align="right">{weatherInfo.rain}</TableCell>
               <TableCell align="right">{weatherInfo.winddirection_10m}</TableCell>
