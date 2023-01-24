181cc2256a1cfa257b42fd3925131ace374aaa40
koarito <amiromarhonein@hotmail.fr>
2023-01-24 14:51:36 +0100

    weather info side done but still has a bug that needs to be adressed!

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,8 @@
 //http://localhost:8080/weather/info?latitude=31.63&longitude=-8.00
 
 import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Stack";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -12,64 +12,99 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import { InputLabel, MenuItem, Select, Typography} from "@mui/material";
 
 function Weather() {
   const [weatherInfo, setWeatherInfo] = useState([]);
   const [selectedCity, setSelectedCity] = useState("Marrakech");
-  var cityLatitude = new Map([
+
+  const cityLatitude = new Map([
     ["Marrakech", 31.63],
     ["Stockholm", 59.33],
     ["Berlin", 52.52]
   ])
-  var cityLongitude = new Map([
+  const cityLongitude = new Map([
     ["Marrakech", -8.00],
     ["Stockholm", 18.07],
     ["Berlin", 13.41]
   ])
   
-  useEffect(() => {
-    loadWeather();
-  });
+const tableData = weatherInfo => {
+  var temp = []
+  weatherInfo.hourly.time.forEach((element, index) => {
+  const object = {
+  time: element,
+  temperature: weatherInfo.hourly.temperature_2m[index],
+  windspeed: weatherInfo.hourly.windspeed_10m[index],
+  winddirection: weatherInfo.hourly.winddirection_10m[index]
+  }
+  temp.push(object)
+ });
+ return temp
+}
+
   const loadWeather = async () => {
     const result = await axios.get(`http://localhost:8080/weather/info?latitude=${cityLatitude.get(selectedCity)}&longitude=${cityLongitude.get(selectedCity)}`, {}, {auth: {
       username: "user",
-      password: "fc5ad5bb-4447-4054-86d4-c1944366c0df"
+      password: "ed6962d8-16f1-42e9-a2eb-886955f49d6c"
     }});
     setWeatherInfo(result.data);
   }
 
-  const handleSetCity = () => {
-
+  useEffect(() => {
+    loadWeather() 
+  },[selectedCity]);
+  const handleCity = e => {
+    setSelectedCity(e.target.value)
   }
   return (
     <div className="App">
-      <Stack direction="row" spacing={2}>
-
-        <Button variant="contained" onClick={() => {console.log(weatherInfo)}}>Set City</Button>
-      </Stack>
-
+      <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+      marginTop="auto"
+      spacing={4}
+    >
+      <Grid item>
+        <Typography variant="h2">Weather Info</Typography>
+      <InputLabel>City</InputLabel>
+  <Select
+    value={selectedCity}
+    label="City"
+    onChange={handleCity}
+  >
+    <MenuItem value={"Marrakech"}>Marrakech</MenuItem>
+    <MenuItem value={"Stockholm"}>Stockholm</MenuItem>
+    <MenuItem value={"Berlin"}>Berlin</MenuItem>
+  </Select>
+  </Grid>
+  <Grid item>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="right">time</TableCell>
-              <TableCell align="right">temperature</TableCell>
-              <TableCell align="right">wind direction</TableCell>
-              <TableCell align="right">windspeed</TableCell>
+              <TableCell align="center">Time({weatherInfo.hourly_units.time})</TableCell>
+              <TableCell align="center">Temperature({weatherInfo.hourly_units.temperature_2m})</TableCell>
+              <TableCell align="center">Windspeed({weatherInfo.hourly_units.windspeed_10m})</TableCell>
+              <TableCell align="center">Wind direction({weatherInfo.hourly_units.winddirection_10m})</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell align="right">{weatherInfo.temperature_2m}</TableCell>
-              <TableCell align="right">{weatherInfo.rain}</TableCell>
-              <TableCell align="right">{weatherInfo.winddirection_10m}</TableCell>
-              <TableCell align="right">{weatherInfo.windspeed_10m}</TableCell>
-            </TableRow>
+            {tableData(weatherInfo).map(row => (
+              <TableRow>
+              <TableCell align="center">{row.time}</TableCell>
+                 <TableCell align="center">{row.temperature}</TableCell>
+                 <TableCell align="center">{row.windspeed}</TableCell>
+                 <TableCell align="center">{row.winddirection}</TableCell>
+                 </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
+      </Grid>
+      </Grid>
     </div>
   );
 }
