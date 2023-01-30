efcb7e65d76cc69785d5ece502cc1691a0a480f3
DenizeScheffold <denizescheffold@gmail.com>
2023-01-30 10:56:28 +0100

    try to make PATCH working. Still not working

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import * as React from "react";
+import axios from "./api/axios";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,23 +7,43 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import EditProfile from './EditProfile';
-
-
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import {Button } from "@mui/material";
 
 function Profile() {
+ 
+  const [profileInfo, setProfileInfo] = React.useState({
+    firstName: "",
+    lastName:"",
+    email: "",
+});
 
-  const [profileInfo, setProfileInfo] = useState([]);
+  const userToPatch = {
+  firstName: profileInfo.firstName, lastName: profileInfo.lastName, email: profileInfo.email
+  };
 
-  useEffect(() =>{
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      'Access-Control-Allow-Origin': '*',
+      }
+    }
+    const handleChange = (prop) => (e) => {
+      setProfileInfo({ ...profileInfo, [prop]: e.target.value });
+    };
+  
+
+  React.useEffect(() =>{
     loadProfile();
   })
   const loadProfile = async () => {
-    const result = await axios.get("http://localhost:8080/user/getuser?email=Elena.Rutsson@gmail.com", {}, {auth: {
+    const result = await axios.get("http://localhost:8080/user/getuser?email=amina@aminas.se", {}, {auth: {
           username: "user",
-          password: "c3014ad4-9f4b-4c09-b4c2-5c3447969f37",
+          password: "a7d2538f-a41c-4c76-9043-3798cc170ac5",
       
         }});
         setProfileInfo(result.data);
@@ -33,7 +53,7 @@ function Profile() {
       const deleteUser = async (id) => {
         await axios.delete(`http://localhost:8080/user/deleteuser?id=${id}`, {}, {auth: {
           username: "user",
-          password: "c3014ad4-9f4b-4c09-b4c2-5c3447969f37"
+          password: "a7d2538f-a41c-4c76-9043-3798cc170ac5"
          
 
       },
@@ -41,6 +61,21 @@ function Profile() {
         loadProfile();
       };
 
+      
+
+  const handleSubmit = () => {
+    const result = axios.patch("http://localhost:8080/user/edituser/252", 
+    {userToPatch}, {config}
+    ,{auth: {
+    username: "user",
+    password: "a7d2538f-a41c-4c76-9043-3798cc170ac5"
+  }});
+ setProfileInfo(result.data);
+  }
+
+
+
+      
 
   return (
 
@@ -86,9 +121,73 @@ function Profile() {
                   >      
                   Delete
                   </button>
-                   
 
-  
+
+
+   <form onSubmit={handleSubmit}>
+    <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+      maxWidth="90%"
+      marginTop="auto"
+      marginLeft="4%"
+      spacing={2}
+    >
+      <Grid item>
+        <Typography variant="h2">Edit Profile</Typography>
+      </Grid>
+
+      <Grid item sx={{ width: 0.5 }}>
+        <FormControl fullWidth>
+          <InputLabel >
+            Förnamn
+          </InputLabel>
+          <OutlinedInput
+            label="Förnamn"
+            value={profileInfo.firstName}
+            onChange={handleChange("firstName")}
+            sx={{ borderRadius: "29px" }}
+          />
+        </FormControl>
+        </Grid>
+        <Grid item sx={{ width: 0.5 }}>
+        <FormControl fullWidth>
+          <InputLabel >
+          Efternamn
+          </InputLabel>
+          <OutlinedInput
+            label="Efternamn"
+            value={profileInfo.lastName}
+            onChange={handleChange("lastName")}
+            sx={{ borderRadius: "29px" }}
+          />
+        </FormControl>
+        </Grid>
+        <Grid item sx={{ width: 0.5 }}>
+        <FormControl fullWidth>
+          <InputLabel>
+            Mailadress
+          </InputLabel>
+          <OutlinedInput
+            label="Mailadress"
+            value={profileInfo.email}
+            onChange={handleChange("email")}
+            sx={{ borderRadius: "29px" }}
+          />
+        </FormControl>
+      </Grid>
+      </Grid>
+      
+      <Button
+            fullWidth
+            variant="contained"
+            type="submit"
+           >
+            Klar
+            </Button>
+    </form>
     </div>
   );
 }
